fc12fee9eea3bc523093bd95265e24f7eab30e18
An Ngoc Lam <an.lam@sintef.no>
2021-09-21 09:26:24 +0200

    Update Evaluation.java

--- owlapi/owl-api-example/src/main/java/no/sintef/skytrack/owlapi/Evaluation.java
+++ owlapi/owl-api-example/src/main/java/no/sintef/skytrack/owlapi/Evaluation.java
@@ -26,12 +26,14 @@ public class Evaluation {
 
 		Map<String, String> ontologiesMap = new LinkedHashMap<String, String>();
 
-		ontologiesMap.put("http://vicodi.org/ontology#", "../../../ontologies/vicodi_all.owl");
-		ontologiesMap.put("http://www.ifomis.org/acgt/1.0#", "../../../ontologies/ACGT.owl");
-		ontologiesMap.put("http://www.co-ode.org/ontologies/galen#", "../../../ontologies/full-galen.owl");
-		ontologiesMap.put("http://purl.org/sig/ont/fma.owl#", "../../../ontologies/fma.owl");
-		ontologiesMap.put("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#", "../../../ontologies/ncit.owl");
+		//ontologiesMap.put("http://vicodi.org/ontology#", "../../../ontologies/vicodi_all.owl");
+		//ontologiesMap.put("http://www.ifomis.org/acgt/1.0#", "../../../ontologies/ACGT.owl");
+		//ontologiesMap.put("http://www.co-ode.org/ontologies/galen#", "../../../ontologies/full-galen.owl");
+		//ontologiesMap.put("http://purl.org/sig/ont/fma.owl#", "../../../ontologies/fma.owl");
+		//ontologiesMap.put("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#", "../../../ontologies/ncit.owl");
+		ontologiesMap.put("http://purl.bioontology.org/ontology/MESH/", "../../../ontologies/MESH.owl");
 		ontologiesMap.put("http://purl.bioontology.org/ontology/MESH/", "../../../ontologies/MESH.ttl");
+		
 		// ontologiesMap.put("http://purl.obolibrary.org/obo/gaz.owl#",
 		// "../../../ontologies/gaz.owl");
 
