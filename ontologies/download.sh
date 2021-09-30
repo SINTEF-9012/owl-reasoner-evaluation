05cde0a1df6d915ae03e5206b82ac1e1d3d2cc1f
An Ngoc Lam <an.lam@sintef.no>
2021-09-30 16:50:56 +0200

    Update download.sh

--- ontologies/download.sh
+++ ontologies/download.sh
@@ -8,17 +8,17 @@ ontoArr["cco.xml"]="https://data.bioontology.org/ontologies/CCO/download?apikey=
 ontoArr["rhmesh.owl"]="https://data.bioontology.org/ontologies/RH-MESH/submissions/3/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
 ontoArr["gexo.owl"]="https://data.bioontology.org/ontologies/GEXO/submissions/2/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
 ontoArr["chebi.xml"]="https://data.bioontology.org/ontologies/CHEBI/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb&download_format=rdf"
-ontoArr["upheno.owl"]="https://data.bioontology.org/ontologies/UPHENO/submissions/3/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
+ontoArr["upheno.xml"]="https://data.bioontology.org/ontologies/UPHENO/submissions/3/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb&download_format=rdf"
 ontoArr["rexo.owl"]="https://data.bioontology.org/ontologies/REXO/submissions/6/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
-ontoArr["nifstd.owl"]="https://data.bioontology.org/ontologies/NIFSTD/submissions/27/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
+ontoArr["nifstd.xml"]="https://data.bioontology.org/ontologies/NIFSTD/submissions/27/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb&download_format=rdf"
 ontoArr["reto.owl"]="https://data.bioontology.org/ontologies/RETO/submissions/8/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
 ontoArr["iobc.owl"]="https://data.bioontology.org/ontologies/IOBC/submissions/25/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
-ontoArr["hoom.owl"]="https://data.bioontology.org/ontologies/HOOM/submissions/4/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
+ontoArr["hoom.xml"]="https://data.bioontology.org/ontologies/HOOM/submissions/4/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb&download_format=rdf"
 ontoArr["ochv.owl"]="https://data.bioontology.org/ontologies/OCHV/submissions/2/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
 ontoArr["snmi.owl"]="https://data.bioontology.org/ontologies/SNMI/submissions/18/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb"
 ontoArr["gaz.xml"]="https://data.bioontology.org/ontologies/GAZ/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb&download_format=rdf"
 
 for key in "${!ontoArr[@]}"; do
     echo "$key ${ontoArr[$key]}"
-	wget -O $key \"$ontoArr[$key]\"
+    wget "${ontoArr[$key]}" -O "$key"
 done
