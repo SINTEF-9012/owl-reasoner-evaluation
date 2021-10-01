494aba2b961d25cffaea2424c6535f2511deca81
An Ngoc Lam <an.lam@sintef.no>
2021-10-01 10:15:29 +0200

    Update evaluation.sh

--- Standalone_Reasoners/Konclude/evaluation.sh
+++ Standalone_Reasoners/Konclude/evaluation.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-declare -a ontoArr=("../../ontologies/vicodi_all.owl"
-					"../../ontologies/ACGT.owl"
-					"../../ontologies/full-galen.owl"
-					"../../ontologies/fma.owl")
+#declare -a ontoArr=("../../ontologies/vicodi_all.owl"
+#					"../../ontologies/ACGT.owl"
+#					"../../ontologies/full-galen.owl"
+#					"../../ontologies/fma.owl")
 #					"../../ontologies/ncit.owl"
 #					"../../ontologies/MESH.owl")
 
 NEWLINE=$'\n'
 
+rm -rf ./output/
 #for i in `ls -Sr ../../ontologies/*`;
 
 echo "Evaluating reasoner Konclude"
@@ -16,7 +17,8 @@ echo
 echo "Evaluating Reasoner consistency validation"
 echo
 output=""
-for i in "${ontoArr[@]}"
+#for i in "${ontoArr[@]}"
+for i in `ls -Sr ../../ontologies/*`;
 do
    echo "$i"
    echo
@@ -37,27 +39,61 @@ do
 	echo "Everage consistency validation time on: $i is $reasonerConsistencyTime"
 done
 
-echo "$output" >> ./output/consistency.csv
+echo "$output" > ./output/consistency.csv
 
 
 
 echo
 echo "Evaluating Reasoner Classification"
 echo
-for i in "${ontoArr[@]}"
+output=""
+for i in `ls -Sr ../../ontologies/*`;
 do
    echo "$i"
    echo
+   output="$output$i"
    reasonerClassification=0
    for runC in {1..10}; do
 		start=$(date +%s%3N)
 		Konclude classification -i $i -o ./output/classification.owl.xml > ./output/classification.log
 		end=$(date +%s%3N)
 		runtime=$( echo "$end - $start" | bc -l )
+		output="${output},${runtime}"
 		reasonerClassification=$(echo "$runtime + $reasonerClassification" | bc -l)
 		echo "Classification takes ${runtime}"
 	done
+	output="${output}${NEWLINE}"
 	
 	reasonerClassification=$(echo "$reasonerClassification/5" | bc -l)
 	echo "Everage classification time on: $i is $reasonerClassification"
 done
+
+echo "$output" > ./output/classification.csv
+
+
+echo
+echo "Evaluating Reasoner Realization"
+echo
+output=""
+for i in `ls -Sr ../../ontologies/*`;
+do
+   echo "$i"
+   echo
+   output="$output$i"
+   reasonerClassification=0
+   for runC in {1..10}; do
+		start=$(date +%s%3N)
+		Konclude classification -i $i -o ./output/realization.owl.xml > ./output/realization.log
+		end=$(date +%s%3N)
+		runtime=$( echo "$end - $start" | bc -l )
+		output="${output},${runtime}"
+		reasonerClassification=$(echo "$runtime + $reasonerClassification" | bc -l)
+		echo "Classification takes ${runtime}"
+	done
+	output="${output}${NEWLINE}"
+	
+	reasonerClassification=$(echo "$reasonerClassification/5" | bc -l)
+	echo "Everage classification time on: $i is $reasonerClassification"
+done
+
+echo "$output" > ./output/realization.csv
