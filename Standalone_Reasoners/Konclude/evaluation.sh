1141ae84f0893987de12c7a49f1b838db6ba78c5
An Ngoc Lam <an.lam@sintef.no>
2021-10-01 09:59:09 +0200

    Update evaluation.sh

--- Standalone_Reasoners/Konclude/evaluation.sh
+++ Standalone_Reasoners/Konclude/evaluation.sh
@@ -3,9 +3,11 @@
 declare -a ontoArr=("../../ontologies/vicodi_all.owl"
 					"../../ontologies/ACGT.owl"
 					"../../ontologies/full-galen.owl"
-					"../../ontologies/fma.owl"
-					"../../ontologies/ncit.owl"
-					"../../ontologies/MESH.owl")
+					"../../ontologies/fma.owl")
+#					"../../ontologies/ncit.owl"
+#					"../../ontologies/MESH.owl")
+
+#for i in `ls -Sr ../../ontologies/*`;
 
 echo "Evaluating reasoner Konclude"
 echo
@@ -15,31 +17,38 @@ for i in "${ontoArr[@]}"
 do
    echo "$i"
    echo
+   output="$i"
    reasonerConsistencyTime=0
-   for runC in {1..5}; do
+   for runC in {1..10}; do
 		start=$(date +%s%3N)
-		Konclude consistency -i $i -o ./output/consistency.owl.xml > consistency.log
+		Konclude consistency -i $i -o ./output/consistency.owl.xml > ./output/consistency.log
 		end=$(date +%s%3N)
 		runtime=$( echo "$end - $start" | bc -l )
+		output="${output},${runtime}"
 		reasonerConsistencyTime=$(echo "$runtime + $reasonerConsistencyTime" | bc -l)
 		echo "Consistency validation takes ${runtime}"
 	done
+	output="${output}\n"
 	
 	reasonerConsistencyTime=$(echo "$reasonerConsistencyTime/5" | bc -l)
 	echo "Everage consistency validation time on: $i is $reasonerConsistencyTime"
 done
 
+echo "$output" >> ./output/consistency.csv
 
 
+
+echo
 echo "Evaluating Reasoner Classification"
+echo
 for i in "${ontoArr[@]}"
 do
    echo "$i"
    echo
    reasonerClassification=0
-   for runC in {1..5}; do
+   for runC in {1..10}; do
 		start=$(date +%s%3N)
-		Konclude classification -i $i -o ./output/classification.owl.xml > classification.log
+		Konclude classification -i $i -o ./output/classification.owl.xml > ./output/classification.log
 		end=$(date +%s%3N)
 		runtime=$( echo "$end - $start" | bc -l )
 		reasonerClassification=$(echo "$runtime + $reasonerClassification" | bc -l)
