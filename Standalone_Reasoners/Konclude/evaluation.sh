ad3b2bd50e6aec5e94cdabe66b471e03b8f7dc24
An Ngoc Lam <an.lam@sintef.no>
2021-11-18 16:52:26 +0100

    Update evaluation.sh

--- Standalone_Reasoners/Konclude/evaluation.sh
+++ Standalone_Reasoners/Konclude/evaluation.sh
@@ -61,8 +61,9 @@ then
 	   do 
 			start=$(date +%s.%3N)
 			timeout $min $Konclude consistency -i "$ontoDir$i" -o "./${outDir}/consistency/consistency_${i}" > "./${outDir}/consistency_${i}.log" 
-			end=$(date +%s.%3N)
 			EXIT_STATUS=$?
+			end=$(date +%s.%3N)
+			
 			runtime=$( echo "scale=3; $end - $start" | bc -l )
 			
 			echo $EXIT_STATUS
